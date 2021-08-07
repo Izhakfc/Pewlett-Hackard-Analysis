9cc8773d45f8f78ccfa69920cc991e402ebb9afa
Izhakfc <izhakflores@gmail.com>
2021-08-07 14:16:00 -0500

    Uploading challenge files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,50 @@
--- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (______) _____,
-______,
-______,
-______
+-- Deliverable 1:
+-- Steps 1-7
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles as ti
+	ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
 
-INTO nameyourtable
-FROM _______
-ORDER BY _____, _____ DESC;
+-- Use Dictinct with Orderby to remove duplicate rows
+-- Steps 8 to 14
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+	rt.first_name,
+	rt.last_name,
+	rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+ORDER BY rt.emp_no, rt.to_date DESC;
+
+
+-- Steps 15-21
+SELECT COUNT(emp_no), title
+INTO retiring_titles
+FROM unique_titles 
+GROUP BY title
+ORDER BY COUNT(emp_no) DESC;
+
+-- Deliverable 2:
+SELECT DISTINCT ON(e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.birth_date, 
+	de.from_date, 
+	de.to_date, 
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de
+	ON e.emp_no = de.emp_no
+INNER JOIN titles as ti
+	ON e.emp_no = ti.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+     AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM retirement_titles;
+DROP TABLE retirement_titles CASCADE;
